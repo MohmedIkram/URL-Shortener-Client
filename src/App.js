75fd93e1cb26fda2ebf2b66dad09d8fc5fcdefa5
Mohmed Ikram <53212527+MohmedIkram@users.noreply.github.com>
2021-08-30 10:31:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Body from "./components/Body";
+import LoginPage from "./components/LoginPage";
+import SignUp from "./components/SignUp";
+import UrlShort from "./components/UrlShort";
+import SendEmail from "./components/SendEmail";
+import ResetPassword from "./components/ResetPassword";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ErrorPage from "./components/Pagefournotfour";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Body />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/signUp">
+            <SignUp />
+          </Route>
+          <Route exact path="/forgot-password">
+            <SendEmail />
+          </Route>
+          <Route exact path="/ResetPassword/:resetToken">
+            <ResetPassword />
+          </Route>
+          <Route path="/UrlShort">
+            <UrlShort />
+          </Route>
+          <Route path="/*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ResetPassword.js
@@ -0,0 +1,139 @@
+import * as React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+/** imported components */
+import img1 from "../assets/img1.svg";
+
+/** import from materail ui */
+import {
+  Paper,
+  withStyles,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { DoneOutline } from "@material-ui/icons";
+
+const styles = () => ({
+  container: {
+    padding: "1em",
+    display: "flex",
+    flexDirection: "column",
+    minWidth: "400px",
+    minHeight: "300px",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: "30px",
+    width: "100px",
+  },
+  doneIcon: {
+    width: "2em",
+    height: "2em",
+    marginBottom: "20px",
+  },
+  errorText: {
+    color: "red",
+  },
+});
+
+function ResetPassword({ classes, token }) {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [done, setDone] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = (callback) => (event) => {
+    callback(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPassword("");
+    setConfirmPassword("");
+    if (password !== confirmPassword) setError(true);
+    else {
+      const myData = {
+        confirmPassword,
+      };
+      axios.post(
+        `https://password-reset-guvi.herokuapp.com/users/reset-password/${token}`,
+        myData
+      );
+      console.log(token);
+      setDone(true);
+    }
+  };
+
+  return (
+    <>
+      <Paper className={classes.container}>
+        {done ? (
+          <React.Fragment>
+            <DoneOutline className={classes.doneIcon} />
+            <Typography variant="h5" gutterBottom>
+              All done!
+            </Typography>
+          </React.Fragment>
+        ) : (
+          <form
+            className={classes.container}
+            onSubmit={handleSubmit}
+            autoComplete="off"
+          >
+            <Typography variant="h5" gutterBottom>
+              Create a new password
+            </Typography>
+            <TextField
+              id="password"
+              type="password"
+              required
+              error={error}
+              label="New password"
+              className={classes.textField}
+              value={password}
+              onChange={handleChange(setPassword)}
+              margin="normal"
+            />
+            <TextField
+              id="confirmPassword"
+              type="password"
+              required
+              error={error}
+              label="Confirm new password"
+              className={classes.textField}
+              value={confirmPassword}
+              onChange={handleChange(setConfirmPassword)}
+              margin="normal"
+            />
+            {error ? (
+              <Typography className={classes.errorText} variant="subtitle2">
+                Passwords must match!
+              </Typography>
+            ) : (
+              <React.Fragment />
+            )}
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              className={classes.button}
+              onSubmit={handleSubmit}
+            >
+              Create
+            </Button>
+          </form>
+        )}
+      </Paper>
+      <img
+        src={img1}
+        alt=" img"
+        style={{ width: "700px", height: "500px" }}
+      ></img>
+    </>
+  );
+}
+
+export default withStyles(styles)(ResetPassword);
