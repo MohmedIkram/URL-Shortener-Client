50ae22a890b90c97d3c381227b7f01d1bec01d09
Mohmed Ikram <53212527+MohmedIkram@users.noreply.github.com>
2021-08-30 15:51:51 +0530

    Add files via upload

--- src/components/UrlShort.js
+++ src/components/UrlShort.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import ParticleBackground from "./ParticleBackground";
 
+/** import from materail ui */
 import Button from "@material-ui/core/Button";
 import InputBase from "@material-ui/core/InputBase";
 import Paper from "@material-ui/core/Paper";
 import SearchIcon from "@material-ui/icons/Search";
 import { Typography } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -30,18 +40,54 @@ const useStyles = makeStyles((theme) => ({
   button: {
     width: "83.8px",
   },
+  table: {
+    width: "100%",
+  },
+  searchcontainertable: {
+    // paddingLeft: "200px",
+    // paddingRight: "300px",
+  },
 }));
-export default function UrlShort() {
+
+function UrlShort() {
+  return (
+    <div>
+      <ParticleBackground />
+      <UrlTable />
+    </div>
+  );
+}
+function UrlTable() {
   const classes = useStyles();
-  const UrlData = (e) => setUrl(e.target.value);
-  const [Url, setUrl] = useState();
-  const [FormUrl, setFormUrl] = useState();
+  const UrlData = (e) => setFullUrl(e.target.value);
+  const [FullUrl, setFullUrl] = useState();
+  const [Data, setData] = useState([]);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const myData = {
+      FullUrl,
+    };
+    axios.post(`http://localhost:5000/url/Fullurl/`, myData);
+    axios.get(`http://localhost:5000/url/Fullurl/`).then((response) => {
+      setData(response);
+      console.log(response);
+    });
+  };
+
+  // useEffect(() => {
+  //   Data();
+  // }, []);
 
   return (
     <div className="App">
       <div className={classes.searchcontainer}>
         <Paper variant="outlined">
           <form className={classes.form}>
+            <div className="search-icon">
+              <SearchIcon color="secondary" />
+            </div>
             <InputBase
               className={classes.searchinput}
               onChange={UrlData}
@@ -51,16 +97,61 @@ export default function UrlShort() {
               type="button"
               color="secondary"
               variant="contained"
-              onClick={Url}
+              onClick={handleSubmit}
             >
               Shrink
             </Button>
           </form>
         </Paper>
       </div>
-      <div className={classes.contents}>
-        <Typography>{Url}</Typography>
-      </div>
+
+      {/* table */}
+
+      <TableContainer
+        // component={Paper}
+        className={classes.searchcontainertable}
+      >
+        <Table classLongUrl={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h5" component="h2">
+                  Long-FullUrl
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="h5" component="h2">
+                  Short-FullUrl
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                {/* <Typography variant="h5" component="h2">
+                  Copy
+                </Typography> */}
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              {/* {Data.map((e) => {
+                <TableCell component="th" scope="row">
+                  {e.data.FullUrl}
+                </TableCell>;
+              })} */}
+
+              <TableCell align="right">hello</TableCell>
+              <TableCell align="right">
+                {" "}
+                <Button variant="contained" color="secondary">
+                  Copy link
+                </Button>{" "}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
+
+export default UrlShort;
