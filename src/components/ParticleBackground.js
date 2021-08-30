8231a784cf43fe655d3324e6b5d7faaf865da1f4
Mohmed Ikram <53212527+MohmedIkram@users.noreply.github.com>
2021-08-30 10:30:34 +0530

    Delete src directory

--- src/components/ParticleBackground.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Particles from "react-particles-js";
-import ParticleConfig from "../config/particle-config";
-
-export default function ParticleBackground() {
-  return <Particles params={ParticleConfig}></Particles>;
-}
