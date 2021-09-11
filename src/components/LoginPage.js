52cdec501db6a5471c04addc232c89e85267cba2
mohmedikram123@gmail.com <mohmedikram123@gmail.com>
2021-09-11 23:16:09 +0530

    update

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -73,11 +73,7 @@ export default function LoginPage() {
     // const LOCAL_URL = "http://localhost:5000/users/login";
 
     axios
-      .post(`${API_URL}`, myData,{
-        headers: {
-          authorization: getData.token,
-          Accept: "application/json",
-        },)
+      .post(`${API_URL}`, myData)
       .then((response) => {
         // return  response;
         localStorage.setItem("x-auth-token", JSON.stringify(response.data));
